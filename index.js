6f14921f5bf7e0f2d18183e59533d49d203aa623
Wanderrful <galois@mentlegen.com>
2018-03-25 05:32:26 +0900

    Added CORS support

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.set("port", (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/", api);
 
 /*
